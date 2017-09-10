72732630fef90000b882b8f8def93627dce4f8b4
asen <ayovcheff@gmail.com>
2017-09-10 22:37:44 +0300

    refactoring

--- config/express.ts
+++ config/express.ts
@@ -1,7 +1,8 @@
 import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
+import * as core from 'express-serve-static-core';
 
-export const init = (app) => {
+export const init = (app: core.Express): void => {
   app.use(bodyParser.json());
   app.use(cors({credentials: true, origin: true}));
 };
