b9d302c051a85fbd2a6297d0fb70ca7976bf0252
asen <ayovcheff@gmail.com>
2017-09-10 19:25:11 +0300

    create default route

--- config/routes.ts
+++ config/routes.ts
@@ -1,4 +1,7 @@
-// eslint-disable-next-line no-unused-vars
-export const getRoutes = (app) => {
-  // app.get(route,  controller);
+import * as core from 'express-serve-static-core';
+
+export const getRoutes = (app: core.Express) => {
+  app.get('*',  (req, res): core.Response => {
+    return res.send('<p>Hello world!</p>');
+  });
 };
